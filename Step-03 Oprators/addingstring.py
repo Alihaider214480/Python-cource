d0f139c68534497d2851b8f9bbfbd3a8610abe02
Alihaider214480 <addogarcreation@gmail.com>
2024-08-04 00:28:11 +0500

    step 3&4

--- Step-03 Oprators/addingstring.py
+++ Step-03 Oprators/addingstring.py
@@ -1,9 +1,18 @@
 #in this we will study how to add a variable in a string.
+""""
 Qoute="if you want a better future create it "
 print("tell a famous quote"+""+Qoute)
 #another way to add is comma
 print("tell a famous quote",        Qoute)
 #another way is f string method.
 print(f"tell a famous quote    {Qoute}")
-# adding int in string
 
+"""
+# adding int in string
+age=12
+ #first way
+print("what is your",age)
+#converting in str
+print("what is your"+ str(age))
+#f string 
+print(f"what is your {age}")
---
+++ Step-04 list/list.py
@@ -0,0 +1,14 @@
+fruits=["apple","banana","kiwi"]
+#first method is to check length using "len method"
+print(len(fruits))
+#checking using index method
+print(fruits[0])
+#adding item in list using "append method"
+fruits.append("oranges")
+print(fruits)
+#removing item using removing method
+fruits.remove("oranges")
+print(fruits)
+#modifying list
+fruits[1]="guava"
+print(fruits)
