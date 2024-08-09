6c2ccd556a4651b812d1083095edc21d5073039f
Alihaider214480 <addogarcreation@gmail.com>
2024-08-09 15:58:16 +0500

    addingstring

--- Step-03 Oprators/addingstring.py
+++ Step-03 Oprators/addingstring.py
@@ -3,7 +3,7 @@
 Qoute="if you want a better future create it "
 print("tell a famous quote"+""+Qoute)
 #another way to add is comma
-print("tell a famous quote",        Qoute)
+print("tell a famous quote",Qoute)
 #another way is f string method.
 print(f"tell a famous quote    {Qoute}")
 
