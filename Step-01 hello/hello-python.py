2ee56b7920d6531bd422d377898115984de907eb
Alihaider214480 <addogarcreation@gmail.com>
2024-08-11 10:19:47 +0500

    change

--- Step-01 hello/hello-python.py
+++ Step-01 hello/hello-python.py
@@ -1,2 +1,4 @@
 print("hello python,stay follish stay hungray")
 #it will give "hello python,stay follish stay hungray" result in terminal.
+#python is dynamic language its mean that whatever we give to it it will give a output.
+#it have type hint for the user.
