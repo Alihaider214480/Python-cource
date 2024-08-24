7ebe6101b48b2cbcc798a4dd29128df383a7c444
Alihaider214480 <addogarcreation@gmail.com>
2024-08-24 15:18:12 +0500

    added second method

--- Assignment/second.py
+++ Assignment/second.py
@@ -6,3 +6,14 @@ while x>=0:
      print(name[x],end="")
      x-=1
 
+
+
+
+
+#second method
+"""
+name=input("plz enter your string/number to reverse\n")
+Name=list(name)
+for x in range(len(name)-1,-1,-1):
+    print(Name[x],end="")
+""""
